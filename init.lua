741afc9994149926a2fe0da50aeed878e34969a7
guisaez <guillesaez15@icloud.com>
2025-10-05 19:00:20 -0400

    Add cnext and cprev

--- init.lua
+++ init.lua
@@ -96,6 +96,8 @@ vim.keymap.set('v', '<leader>y', '"+y')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>cn', ':cnext<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>cp', ':cprev<CR>', { noremap = true, silent = true })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
